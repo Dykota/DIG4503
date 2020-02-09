e28eebd7a6894b9cbfb689b32516ed05b438ab94
Dykota <43105455+Dykota@users.noreply.github.com>
2020-02-09 02:58:53 -0500

    Update index.js

--- Lab4/index.js
+++ Lab4/index.js
@@ -5,20 +5,23 @@ const port = 80;
 
 const pokemons = require("json-pokemon");
 const pokemon = pokemons[0];
+const getPokemons = require('json-pokemon/getPokemon');
 
 App.get("/id/:id", (req, res) => {
-    if(0 < req.params.id <= 890) {
-        let x = req.params.id;
-        res.send(console.log(chalk.green(pokemon.getPokemonById.x)));
+    if(0 < parseInt(req.params.id) <= 890) {
+        let id = parseInt(req.params.id);
+        let mon = getPokemons.getPokemonById(id);
+        res.send(console.log(chalk.green(mon)));
     } else {
           res.send(console.log(chalk.red("ID doesn't exist! Do you even Pokemon, bro?")));
     } 
 });
 
 App.get("/name/:name", (req, res) => {
-    if(req.params.name) {
-        let y = req.params.name;
-        res.send(console.log(chalk.green(pokemon.getPokemonByName.y)));
+    if(req.params.name == pokemon.value) {
+        let name = req.params.name;
+        let monn = getPokemons.getPokemonByName(name);
+        res.send(console.log(chalk.green(getPokemons.getPokemonByName(monn))));
     } else {
         res.send(console.log(chalk.red("Name doesn't exist! Do you even Pokemon, bro?")));
     }
